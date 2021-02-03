bc04f4e24d0cf1f3735d410cbb56a8c5339f90c3
ragnar <ragnar@rmgk.de>
2021-02-03 19:20:04 +0100

    wee, stdout crashes \o/

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,9 @@
 package lambdatest
 
-import scribe.Logger
-
 import java.net.URI
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse.BodyHandlers
 import java.net.http.{HttpClient, HttpRequest}
-import scala.util.Random
 
 object Main {
 
@@ -14,21 +11,7 @@ object Main {
     val apiurl = System.getenv("AWS_LAMBDA_RUNTIME_API")
     val client = HttpClient.newHttpClient()
 
-    val vmID = Random.nextLong()
-
-    import scribe.format._
-
-    val normalFormatter: Formatter =
-      formatter"${string(vmID.toString)} $time:$message$mdc [${className.abbreviate(maxLength = 15, padded = false)}]"
-    Logger.root.clearHandlers().withHandler(
-      formatter = normalFormatter,
-      minimumLevel = Some(scribe.Level.Info)
-    ).replace()
-
-    scribe.info(s"started")
-
     while (true) {
-      scribe.info("accepting request")
       val req = HttpRequest.newBuilder().uri(
         URI.create(s"http://${apiurl}/2018-06-01/runtime/invocation/next")
       ).build()
@@ -37,7 +20,6 @@ object Main {
       val reqId     = res.headers().firstValue("Lambda-Runtime-Aws-Request-Id").get()
       val eventData = res.body()
 
-      scribe.info(s"sending response for $reqId")
       val req2 = HttpRequest.newBuilder().uri(
         URI.create(s"http://${apiurl}/2018-06-01/runtime/invocation/$reqId/response")
       ).method("POST", BodyPublishers.ofString(eventData.reverse)).build()
