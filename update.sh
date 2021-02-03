6f9b21563d6932526d0d0174013a41992183b839
ragnar <ragnar@rmgk.de>
2021-02-03 18:42:28 +0100

    add script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/fish
+
+sbt nativeImage
+
+cp target/native-image/awslambda bootstrap
+zip awstest.zip bootstrap
+aws lambda delete-function --function-name scala-runtime
+
+aws lambda create-function --function-name scala-runtime \
+  --zip-file fileb://awstest.zip --handler function.handler --runtime provided \
+  --role arn:aws:iam::601982883386:role/lambda-role
+
+aws lambda invoke --function-name scala-runtime --payload '{"text":"waat4"}' response.txt
+
