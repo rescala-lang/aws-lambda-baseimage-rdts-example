56809cd10d36a671051547e24be080de17b52da6
Philipp Martens <philipp.martens98@gmail.com>
2021-08-30 14:51:58 +0200

    comment out unused values

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,15 +30,15 @@ object Main {
 
     // scribe.info("test")
 
-    val bucketName = "de-tu-darmstadt-stg-crdt"
+    // val bucketName = "de-tu-darmstadt-stg-crdt"
 
-    val deltaStateKey = "deltaState"
+    // val deltaStateKey = "deltaState"
 
-    //val httpClient = ApacheHttpClient.builder().build()
+    // val httpClient = ApacheHttpClient.builder().build()
 
     // val s3 = S3Client.builder().region(Region.EU_CENTRAL_1).build()
 
-    implicit val intCodec: JsonValueCodec[Int] = JsonCodecMaker.make
+    // implicit val intCodec: JsonValueCodec[Int] = JsonCodecMaker.make
 
     implicit val InputCodec: JsonValueCodec[List[Int]] = JsonCodecMaker.make
 
@@ -74,7 +74,7 @@ object Main {
 
       val mutatedSet = set.addAll(addList)
 
-      val deltaState = mutatedSet.deltaBuffer.head.deltaState
+      // val deltaState = mutatedSet.deltaBuffer.head.deltaState
 
       println(s"mutated set: ${mutatedSet.elements}")
 
